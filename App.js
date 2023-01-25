a26723211ac090c4067301a824d5851191a0c929
Labir852 <tanvir.labir@gmail.com>
2023-01-25 23:49:04 +0600

    done with first page design but styling left

--- App.js
+++ App.js
@@ -9,46 +9,98 @@ import React from 'react';
 
 
 
-import type {PropsWithChildren} from 'react';
 import {
   Alert,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
 import { Provider as PaperProvider } from 'react-native-paper';
-import { Appbar, TextInput } from 'react-native-paper';
+import { Appbar, Text,IconButton, MD3Colors,SegmentedButtons} from 'react-native-paper';
 
 function App(){
 
-  
+  const [text, setText] = React.useState("");
+  const [value, setValue] = React.useState('');
   return (
 
-      <View style={{backgroundColor:"#ffffff"}}>
+      <ScrollView>
+      <View 
+      style={styles.container}>
+
         <PaperProvider>
           <Appbar style={{backgroundColor:"red"}}>
             <Appbar.Content title="Demo App"/>
             <Appbar.Action icon="magnify" onPress={() => {}} />
           </Appbar>
+          <Text style={styles.sectionTitle}>
+          মোট বকেয়া:
+          </Text>
+          <Text style={styles.sectionCalculation}>
+          0.00
+          </Text>
+          <Text style={styles.sectionTitle}>
+          মোট আদায়কৃত:
+          </Text>
+           
+          <Text style={styles.sectionCalculation}>
+          0.00
+          </Text>
+          <Text style={styles.sectionTitle}>
+          মোট বকেয়াকারী :
+          </Text>
+          <Text style={styles.sectionCalculation}>
+          0.00
+          </Text>
+          <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        buttons={[
+          {
+            value: 'বকেয়াকারী',
+            label: 'বকেয়াকারী',
+          },
+          { value: 'পরিশোধকারী', label: 'পরিশোধকারী' },
+        ]}
+      />
+
+      <Text>
+      এই মুহূর্তে আপনার কোনো বকেয়াকারী নেই।  নতুন বকেয়াকারী এড করতে হলে + বাটনটি চাপুন এবং প্রয়োজনীয় তথ্য পূরণ করুন। 
+      </Text>
+      <IconButton
+    icon="plus"
+    iconColor="white"
+    size={20}
+    containerColor="red"
+    onPress={() => console.log('Pressed')}
+  />
         </PaperProvider>
+
+        
         </View>
+        </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   sectionTitle: {
-    fontSize: 24,
+    marginTop: 50,
+    border:0,
+    fontSize: 25,
     fontWeight: '600',
+  },
+  sectionCalculation: {
+    color:'red',
+    paddingTop:0,
+    marginTop:80,
+    fontSize: 30,
+    fontWeight: '600',
+    
   },
   sectionDescription: {
     marginTop: 8,
@@ -57,6 +109,10 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+    
+  },
+  container: {
+    flex: 1,
   },
 });
 
